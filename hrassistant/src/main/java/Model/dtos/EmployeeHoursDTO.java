0b482c55a44a7bd951779e316e749bf3e197a69d
Brasovean Dan <61885079+brasoveandan@users.noreply.github.com>
2020-12-13 17:15:39 +0200

    ChangeV3

--- hrassistant/src/main/java/Model/dtos/EmployeeHoursDTO.java
+++ hrassistant/src/main/java/Model/dtos/EmployeeHoursDTO.java
@@ -1,16 +1,40 @@
 package Model.dtos;
 
 public class EmployeeHoursDTO {
+    private String year;
+    private String month;
     private float workedHours;
     private float requiredHours;
     private float overtimeHours;
     private float overtimeLeave;
 
-    public EmployeeHoursDTO(float workedHours, float requiredHours, float overtimeHours, float overtimeLeave) {
+    public EmployeeHoursDTO(String year, String month, float workedHours, float requiredHours, float overtimeHours, float overtimeLeave) {
+        this.year = year;
+        this.month = month;
         this.workedHours = workedHours;
         this.requiredHours = requiredHours;
         this.overtimeHours = overtimeHours;
         this.overtimeLeave = overtimeLeave;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public float getWorkedHours() {
+        return workedHours;
     }
 
     public void setWorkedHours(float workedHours) {
@@ -23,6 +47,10 @@ public class EmployeeHoursDTO {
 
     public void setRequiredHours(float requiredHours) {
         this.requiredHours = requiredHours;
+    }
+
+    public float getOvertimeHours() {
+        return overtimeHours;
     }
 
     public void setOvertimeHours(float overtimeHours) {
