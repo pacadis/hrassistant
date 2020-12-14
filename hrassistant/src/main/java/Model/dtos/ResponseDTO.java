71e4b5923bebee534da2f940b1b9a76cfcb48a38
Brasovean Dan <61885079+brasoveandan@users.noreply.github.com>
2020-12-15 00:53:44 +0200

    LastUpdate

---
+++ hrassistant/src/main/java/Model/dtos/ResponseDTO.java
@@ -0,0 +1,17 @@
+package Model.dtos;
+
+public class ResponseDTO {
+    private String result;
+
+    public ResponseDTO(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
